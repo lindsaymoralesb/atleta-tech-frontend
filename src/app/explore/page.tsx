777934ed6d8a1938853521712864dd9624c1770c
Lindsay Morales <lindsaymorales15@gmail.com>
2024-10-18 20:32:10 -0600

    update explore page styles

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,13 +1,15 @@
-// src/app/explore/page.tsx
+"use client";
+
 import React from 'react';
 import NFTDetails from '../components/NFTDetails';
+import Navbar from '../components/Navbar';
 
 const ExplorePage = () => {
   const collectionId = 'BASE:0x7498fbc9295aaae8c886d35e850161e62e11768e';
 
   return (
-    <div>
-      <h1>Explore NFT Collection</h1>
+    <div className="min-h-screen bg-gradient-to-br from-[#001838] to-[#bcc6e9] flex flex-col">
+      <Navbar />
       <NFTDetails collectionId={collectionId} /> {/* Change itemId to collectionId */}
     </div>
   );
